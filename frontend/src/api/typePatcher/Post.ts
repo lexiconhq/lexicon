e28f0e594eaa002370011562fd20ea17ee825110
starjustice <stephenmulyadi97@gmail.com>
2025-04-10 14:40:49 +0700

    delete case file name

--- frontend/src/api/typePatcher/Post.ts
+++
@@ -1,7 +0,0 @@
-import { RestLink } from 'apollo-link-rest';
-
-import { generatePostPatcher } from './helper/Post';
-
-export const PostPatcher: RestLink.FunctionalTypePatcher = (post) => {
-  return generatePostPatcher(post);
-};
